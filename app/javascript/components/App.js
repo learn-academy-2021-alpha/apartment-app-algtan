9c349903fef3132f1f042b3c22d35aef9124ce33
algtan <agtan@ymail.com>
2021-04-06 16:01:45 -0700

    created seeds for users and apartments; able to show index of apartments

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,40 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import Home from './pages/Home'
+import ApartmentIndex from './pages/ApartmentIndex'
+
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apartments: []
+    }
+  }
+
+  componentDidMount(){
+    this.apartmentIndex()
+  }
+
+  apartmentIndex = () => {
+    fetch("http://localhost:3000/apartments")
+    .then(response => {
+      return response.json()
+    })
+    .then(apartmentsArray => {
+      this.setState({ apartments: apartmentsArray })
+    })
+    .catch(errors => {
+      console.log("index errors:", errors)
+    })
+  }
+
   render() {
     const {
       logged_in,
@@ -21,6 +55,14 @@ class App extends React.Component {
             <a href={ sign_in_route }>Sign In</a>
           </div>
         }
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/index" render={ (props) => <ApartmentIndex apartments={ this.state.apartments } /> } />
+          </Switch>
+        </Router>
+
       </React.Fragment>
     )
   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user1 = User.new
+# user1.email = 'curt@email.com'
+# user1.encrypted_password = 'password'
+# user1.save
+
+# user2 = User.new
+# user2.email = 'leslie@email.com'
+# user2.encrypted_password = 'password'
+# user2.save
+
+users = [
+    {
+        email: 'curt@email.com',
+        password: 'password'
+    },
+    {
+        email: 'leslie@email.com',
+        password: 'password'
+    }
+]
+
+users.each do |attributes|
+    User.create attributes
+end
+
+apartments = [
+    {
+        street: '4th Ave',
+        city: 'San Diego',
+        state: 'CA',
+        manager: 'Curt',
+        email: 'curt@email.com',
+        price: '$1300',
+        bedrooms: 1,
+        bathrooms: 1,
+        pets: 'Not allowed',
+        user: User.first
+    },
+    {
+        street: 'Rancho Mission Road',
+        city: 'San Diego',
+        state: 'CA',
+        manager: 'Leslie',
+        email: 'leslie@email.com',
+        price: '$1600',
+        bedrooms: 2,
+        bathrooms: 1,
+        pets: 'Not allowed',
+        user: User.second
+    }
+]
+
+apartments.each do |attributes|
+    Apartment.create attributes
+end
